d2c4984f9958178b6e1f235dcc52ab6b009f2b24
TomPope94 <48588640+TomPope94@users.noreply.github.com>
2020-05-19 19:30:56 +0100

    changes logo to be shapes not text

--- src/components/global/Logo.js
+++ src/components/global/Logo.js
@@ -10,35 +10,26 @@ const Logo = ({ ...props }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="188.47"
-      height="38.62"
-      viewBox="0 0 188.47 38.62"
+      width="177.76"
+      height="29.98"
+      viewBox="0 0 177.76 29.98"
       style={styles.logoContainer}
       {...props}
     >
-      <text
+      <path
         fill="#0396a6"
-        fontFamily="HWTMardell, HWT Mardell"
-        fontSize="36"
-        transform="translate(0 30.82)"
-      >
-        <tspan letterSpacing="-.04em">C</tspan>
-        <tspan x="21.56" y="0">
-          c
-        </tspan>
-        <tspan x="44.68" y="0" fill="#d95a11" letterSpacing="-.04em">
-          G
-        </tspan>
-        <tspan x="70.88" y="0" fill="#d95a11" letterSpacing="0em">
-          aming
-        </tspan>
-      </text>
+        d="M20.95 23.47a12.52 12.52 0 01-8.14 3A13 13 0 010 13.36 12.94 12.94 0 0112.81.26a12.51 12.51 0 018.14 3l-8.14 10.1zM42.51 23.51a12.51 12.51 0 01-8.13 3 13 13 0 01-12.82-13.12A13 13 0 0134.39.29a12.51 12.51 0 018.13 3l-8.13 10.1z"
+      ></path>
+      <path
+        fill="#d95a11"
+        d="M70.27 14.8a12.83 12.83 0 01-25.6-1.41A13 13 0 0157.52.29a12.52 12.52 0 018.14 3L56.48 14.8zM81.46-.02l10.93 26.19H70.52zM106.77 14.83L118.65.11v26.06H94.89V.11zM125.78 26.17h-3.53V.54h3.53zM149.29.54v25.63h-19.9V.11l16.38 20.48V.54zM177.76 14.76a12.82 12.82 0 01-25.59-1.4A13 13 0 01165.02.26a12.48 12.48 0 018.13 3l-9.18 11.52z"
+      ></path>
       <path
         fill="none"
         stroke="#038c5a"
         strokeMiterlimit="10"
         strokeWidth="2"
-        d="M2.47 33.62L175.47 33.62"
+        d="M1.39 28.98L174.39 28.98"
       ></path>
     </svg>
   );
